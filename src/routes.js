86ba17c9c980e5a08cea163d72647dd827d4a494
LuNe137 <luizferreirafariasn@gmail.com>
2022-12-10 15:00:53 -0300

    fixing navbar and footer bugs they were not appearing

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,12 @@
 import { BrowserRouter, Route, Routes} from 'react-router-dom';
-import PaginaPadrao from 'components/PaginaPadrao';
+import PaginaPadrao from 'components/PaginaPadrao/index';
 import Home from 'pages/Home';
 
 export default function Router(){
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' elemente={<PaginaPadrao />}>
+        <Route path='/' element={<PaginaPadrao />}>
           <Route index element={<Home />}/>
         </Route>
       </Routes>
