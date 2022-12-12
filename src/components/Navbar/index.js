b920b981c3a50908739a27bcf689e9fc21c7ada0
LuNe137 <luizferreirafariasn@gmail.com>
2022-12-12 19:40:39 -0300

    router dom added, redux added, categories and items page added, home and navbar update

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,6 +3,7 @@ import styles from './Navbar.module.scss';
 import {ReactComponent as Logo} from '../../assets/logo.svg';
 import classNames from 'classnames';
 import { RiShoppingCart2Line, RiShoppingCartFill } from 'react-icons/ri';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 
 const iconeProps = {
   color: 'white',
@@ -10,28 +11,31 @@ const iconeProps = {
 }
 
 export default function Navbar() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
   return (
     <nav className={styles.nav}>
-      <Logo className={styles.logo}/>
+      <Logo className={styles.logo} onClick={() => navigate('/')}/>
       <div className={styles.links}>
         <div>
-          <a href='/' className={classNames(styles.link, {
-            [styles.selected]: window.location.pathname === '/'
+          <Link to='/' className={classNames(styles.link, {
+            [styles.selected]: location.pathname === '/'
           })}>
             Página inicial
-          </a>
+          </Link>
         </div>
       </div>
       <div className={styles.busca}>
         <Busca />
       </div>
       <div className={styles.icones}>
-        <a href='/carrinho'>
+        <Link to='/carrinho'>
           { window.location.pathname === '/carrinho'
            ? <RiShoppingCartFill {...iconeProps}/>
            : <RiShoppingCart2Line {...iconeProps}/>
           }
-        </a>
+        </Link>
       </div>
     </nav>
   );
