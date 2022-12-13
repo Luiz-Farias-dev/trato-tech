8fbb708f61a4d4dced6633c74c6cd0d94ed52470
LuNe137 <luizferreirafariasn@gmail.com>
2022-12-13 16:07:18 -0300

    cart page created and updated

--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -1,8 +1,10 @@
 import styles from './Item.module.scss';
 import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai';
 import { FaCartPlus } from 'react-icons/fa';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { mudarFavorito } from 'store/reducers/itens';
+import { mudarCarrinho } from 'store/reducers/carrinho';
+import classNames from 'classnames';
 
 const iconProps = {
   size: 24,
@@ -16,17 +18,25 @@ export default function Item(props) {
     preco,
     descricao,
     favorito,
-    id
+    id,
+    carrinho
   } = props;
 
   const dispatch = useDispatch();
+  const estaNoCarrinho = useSelector(state => state.carrinho.some(itemNoCarrinho => itemNoCarrinho.id === id))
 
   const resolverFavorito = () => {
     dispatch(mudarFavorito(id));
   }
 
+  const resolverCarrinho = () => {
+    dispatch(mudarCarrinho(id))
+  }
+
   return (
-    <div className={styles.item}>
+    <div className={classNames(styles.item, {
+      [styles.itemNoCarrinho]: carrinho
+    })}>
       <div className={styles['item.imagem']}>
         <img src={foto} alt={titulo} />
       </div>
@@ -44,7 +54,7 @@ export default function Item(props) {
           ? <AiFillHeart {...iconProps} color='#ff0000' className={styles['item-acao']} onClick={resolverFavorito}/>
           : <AiOutlineHeart  {...iconProps} className={styles['item-acao']} onClick={resolverFavorito}/>
           }
-          <FaCartPlus {...iconProps} color={false ? '#1875E8' : iconProps.color} className={styles['item-acao']} />
+          <FaCartPlus {...iconProps} color={estaNoCarrinho ? '#1875E8' : iconProps.color} className={styles['item-acao']} onClick={resolverCarrinho}/>
         </div>
       </div>
       </div>
